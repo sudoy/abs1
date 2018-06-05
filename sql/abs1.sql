2e79b380ccbabf080d9d19422b96e39f95b21807
hayate-sasahara <hayate-sasahara@tsd-201511-0001>
2018-06-05 13:55:55 +0900

    SQL 外部テーブル作成

--- sql/abs1.sql
+++ sql/abs1.sql
@@ -2,20 +2,36 @@ CREATE DATABASE abs1;
 
 CREATE TABLE abs1(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	date DATE NOT NULL, 
-	category VARCHAR(100) NOT NULL,
+	date DATE NOT NULL,
+	category INT NOT NULL,
 	note VARCHAR(100),
 	price int NOT NULL
 );
 
 
+INSERT INTO abs1(date, category, note, price)
+VALUES('2018-05-29', 1, 'ティッシュペーパー、歯磨き粉など', -740);
+INSERT INTO abs1(date, category, note, price)
+VALUES('2018-05-29', 2, '', -800);
+INSERT INTO abs1(date, category, note, price)
+VALUES('2018-05-30', 4, '', 120000);
+INSERT INTO abs1(date, category, note, price)
+VALUES('2018-05-30', 5, 'ラッキー', 3000);
 
-INSERT INTO abs1(date, category, note, price)
-VALUES('2018-05-29', '日用品', 'ティッシュペーパー、歯磨き粉など', -740);
-INSERT INTO abs1(date, category, note, price)
-VALUES('2018-05-29', '交際費', '', -800);
-INSERT INTO abs1(date, category, note, price)
-VALUES('2018-05-30', 'アルバイト代', '', 120000);
-INSERT INTO abs1(date, category, note, price)
-VALUES('2018-05-30', '宝くじ', '', 3000);
+
+CREATE TABLE list(
+	category_id INT PRIMARY KEY AUTO_INCREMENT,
+	category_data VARCHAR(100)
+);
+
+INSERT INTO list(category_data)
+VALUES('食費');
+INSERT INTO list(category_data)
+VALUES('日用品');
+INSERT INTO list(category_data)
+VALUES('交際費');
+INSERT INTO list(category_data)
+VALUES('アルバイト代');
+INSERT INTO list(category_data)
+VALUES('宝くじ');
 
